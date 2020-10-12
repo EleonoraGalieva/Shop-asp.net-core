6c77fb636609900267a5258ac44f8044f267fad3
EleonoraGalieva <48092813+EleonoraGalieva@users.noreply.github.com>
2020-10-12 10:37:09 +0300

    Models & controllers added
    Added two models & controllers for them

---
+++ BakeryShop/BakeryShop/Controllers/PieController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BakeryShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BakeryShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+            _pieRepository = pieRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(_pieRepository.AllPies);
+        }
+    }
+}
