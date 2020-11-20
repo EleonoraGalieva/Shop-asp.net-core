96d7d0b672babce96764bdaac68f4f2d075f2e89
EleonoraGalieva <48092813+EleonoraGalieva@users.noreply.github.com>
2020-11-20 16:45:53 +0300

    logs for error pages added

--- BakeryShop/BakeryShop/Controllers/ErrorController.cs
+++ BakeryShop/BakeryShop/Controllers/ErrorController.cs
@@ -5,29 +5,43 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace BakeryShop.Controllers
 {
     public class ErrorController : Controller
     {
-        [Route("Error/{statusCode}")]   
+        private readonly ILogger<ErrorController> _logger;
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("Error/{statusCode}")]
         public IActionResult HttpStatusCodeHandler(int statusCode)
         {
             switch (statusCode)
             {
                 case 404:
-                    ViewBag.Message = "Sorry, the page is not found."; break;
+                    ViewBag.Message = "Sorry, the page is not found.";
+                    _logger.LogWarning($"404 error occured.");
+                    break;
                 default:
                     ViewBag.Message = "Something went wrong..."; break;
             }
             return View("ErrorPage");
         }
+
         [Route("Error")]
         [AllowAnonymous]
         public IActionResult Error()
         {
             var exceptionDetails = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
-            ViewBag.ExceptionMessage = exceptionDetails.Error.Message;
+
+            _logger.LogError($"The path {exceptionDetails.Path} " +
+                $"threw an exception {exceptionDetails.Error}");
+
             return View("Error");
         }
     }
